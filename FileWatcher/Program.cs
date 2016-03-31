b76185f0b1d95152728d3be10d1b2af9e4d84065
sivaganeshan <sivaganeshan36@gmail.com>
2016-03-31 11:23:46 +0530

    Added necessary files via upload

---
+++ FileWatcher/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\ConfigFilesFareBox02022016\ConfigFilesFareBox02022016\";
+            Program prg = new Program();
+            prg.CreateFileWatcher(filePath);
+            while (true)
+            {
+                //
+               
+            }
+        }
+
+        public void CreateFileWatcher(string path)
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            /* Watch for changes in LastAccess and LastWrite times, and 
+               the renaming of files or directories. */
+            watcher.NotifyFilter = /*NotifyFilters.LastAccess |*/ NotifyFilters.LastWrite;
+              // | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.xml";
+
+            // Add event handlers.
+            
+                watcher.Changed += new FileSystemEventHandler(OnChanged);
+
+            
+           
+           // watcher.Created += new FileSystemEventHandler(OnChanged);
+            //watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            //watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        // Define the event handlers.
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+    }
+}
